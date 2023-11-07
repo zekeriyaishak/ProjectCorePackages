97f48223575df424a4ffe7707b3e3703a073c797
ZEKERİYA İSHAK DOĞANAY <zishakdoganay@ogr.trakya.edu.tr>
2023-11-07 20:34:18 +0300

    Pageable feature added

---
+++ ProjectCore.DataAccess/Dynamic/Filter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectCore.DataAccess.Dynamic;
+
+public class Filter
+{
+    public string Field { get; set; } // where
+    public string? Value { get; set; }
+    public string Operator { get; set; } // eşittir
+    public string? Logic { get; set; } // and or
+    public IEnumerable<Filter> Filters { get; set; }
+    public Filter()
+    {
+        Field = string.Empty;
+        Operator = string.Empty;
+    }
+    public Filter(string field, string @operator)
+    {
+        Field = field;
+        Operator = @operator;
+    }
+}
