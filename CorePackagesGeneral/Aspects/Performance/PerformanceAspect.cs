98a68c64847122044c7f98f9ceaec373df1cb17d
ZEKERİYA İSHAK DOĞANAY <zishakdoganay@ogr.trakya.edu.tr>
2024-06-22 17:40:15 +0300

    TransactionScope Test added and fixed performance aspect

--- CorePackagesGeneral/Aspects/Performance/PerformanceAspect.cs
+++ CorePackagesGeneral/Aspects/Performance/PerformanceAspect.cs
@@ -32,7 +32,7 @@ public class PerformanceAspect : MethodInterception
     {
         if (_stopwatch.Elapsed.TotalSeconds > _interval)
         {
-            Debug.WriteLine($"Performance : {invocation.Method.DeclaringType.FullName}.{invocation.Method.Name}-->{_stopwatch.Elapsed.TotalSeconds}");
+            Console.WriteLine($"Performance : {invocation.Method.DeclaringType.FullName}.{invocation.Method.Name}-->{_stopwatch.Elapsed.TotalSeconds}");
         }
         _stopwatch.Reset();
     }
